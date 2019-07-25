932bb1774f2dd8743e35226c3293008d1b6b513a
Bernardo <behornerl@gmail.com>
2019-07-25 00:46:47 -0300

    Movimento 3D + câmera em 3a pessoa

---
+++ Oficina Introdução Unity/Assets/Scripts/CameraTerceiraPessoa.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTerceiraPessoa : MonoBehaviour
+{
+    [Tooltip("Se o cursor do mouse deve ficar travado ou livre")]
+    [SerializeField] private bool travarCursor;
+    [Tooltip("A sensibilidade do movimento da câmera baseado no mouse")]
+    [SerializeField] private float sensibidadeMouse = 10;
+    [Tooltip("GameObject que a câmera orbitará em volta (centro da órbita)")]
+    [SerializeField] private Transform centroRotacaoCamera;
+    [Tooltip("Distância que a câmera ficará do centro da órbita")]
+    [SerializeField] private float distanciaDoCentro = 2;
+    [Tooltip("Mínimo (x) e máximo (y) da inclinação da câmera")]
+    [SerializeField] private Vector2 minMaxInclinacao = new Vector2(-40, 85);
+
+    // É o tempo que a câmera demorará para concluir a rotação indicada pelo movimento do mouse.
+    // Esse atraso dá uma sensação de um movimento mais suave do que se fosse imediato
+    [Tooltip("Nível de suavidade do movimento da câmera")]
+    [SerializeField] private float tempoDeSuavizacaoRotacao = .12f;
+    Vector3 velocidadeDaSuavizacaoRotacao;
+    Vector3 rotacaoAtual;
+
+    float guinada;      // Rotação na horizontal
+    float inclinacao;   // Rotação na vertical
+
+    void Start()
+    {
+        if (travarCursor)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+    }
+
+    void LateUpdate()
+    {
+        guinada += Input.GetAxis("Mouse X") * sensibidadeMouse;
+        inclinacao -= Input.GetAxis("Mouse Y") * sensibidadeMouse;
+        inclinacao = Mathf.Clamp(inclinacao, minMaxInclinacao.x, minMaxInclinacao.y);
+
+        rotacaoAtual = Vector3.SmoothDamp(rotacaoAtual, new Vector3(inclinacao, guinada), 
+            ref velocidadeDaSuavizacaoRotacao, tempoDeSuavizacaoRotacao);
+
+        transform.eulerAngles = rotacaoAtual;
+        transform.position = centroRotacaoCamera.position - transform.forward * distanciaDoCentro;
+    }
+}
+
---
+++ Oficina Introdução Unity/Assets/Scripts/Movimento3D.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class Movimento3D : MonoBehaviour
+{
+    [Tooltip("Câmera que será usada para")]
+    public Camera cam;
+
+    [Tooltip("Nome do input para mover o objeto para frente e para trás")]
+    [SerializeField] private string inputHorizontal;
+    [Tooltip("Nome do input para mover o objeto para esquerda e direita")]
+    [SerializeField] private string inputVertical;
+
+    [Tooltip("A força aplicada para manter o jogador colado ao chão em uma descida enclinada")]
+    public float forcaRampa;
+    [Tooltip("O comprimento do raio que checa se o jogador está em uma rampa ou não")]
+    public float comprimentoRaioForcaRampa;
+
+    [Tooltip("A taxa de diminuição de força que o pulo sofrerá até seu ápice")]
+    public AnimationCurve decrescimentoDoPulo;
+    [Tooltip("A 'força' do pulo")]
+    public float multiplicadorPulo;
+    [Tooltip("A tecla usada para pular")]
+    public KeyCode teclaDePulo;
+
+
+    private bool isJumping;
+
+    [Tooltip("Velocidade com a qual o objeto se moverá")]
+    public float velocidadeMovimento;
+
+    private CharacterController charController;
+
+    private void Awake()
+    {
+        charController = GetComponent<CharacterController>();
+        cam = GetComponentInChildren<Camera>();
+    }
+
+    private void Update()
+    {
+        float horizInput = Input.GetAxis(inputHorizontal);
+        float vertInput = Input.GetAxis(inputVertical);
+
+        charController.SimpleMove(Vector3.ClampMagnitude((vertInput * cam.transform.forward +
+                                    horizInput * cam.transform.right) * velocidadeMovimento , 1.0f) * velocidadeMovimento);
+
+        if((horizInput != 0 || vertInput != 0) && CheckSlope())
+        {
+            charController.Move(Vector3.down * charController.height / 2 * forcaRampa * Time.deltaTime);
+        }
+
+        if(Input.GetKeyDown(teclaDePulo) && !isJumping)
+        {
+            isJumping = true;
+            StartCoroutine(JumpEvent());
+        }
+    }
+
+    private bool CheckSlope()
+    {
+        if (isJumping)
+            return false;
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, Vector3.down, out hit, charController.height / 2 * comprimentoRaioForcaRampa))
+            if (hit.normal != Vector3.up)
+                return true;
+
+        return false;
+    }
+
+    private IEnumerator JumpEvent()
+    {
+        float timeInAir = 0f;
+        do
+        {
+            float jumpForce = decrescimentoDoPulo.Evaluate(timeInAir);
+            charController.Move(Vector3.up * jumpForce * multiplicadorPulo * Time.deltaTime);
+            timeInAir += Time.deltaTime;
+            yield return null;
+        } while (!charController.isGrounded && charController.collisionFlags != CollisionFlags.Above);
+
+        charController.slopeLimit = 90.0f;
+        isJumping = false;
+    }
+}
