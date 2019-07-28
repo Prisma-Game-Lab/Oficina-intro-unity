06b371f40fa692f2a3f6e5d3aff43972ab0fd09e
Bernardo <behornerl@gmail.com>
2019-07-28 00:37:42 -0300

    update movimentação 3D

--- Oficina Introdução Unity/Assets/Scripts/Movimento3D.cs
+++ Oficina Introdução Unity/Assets/Scripts/Movimento3D.cs
@@ -8,23 +8,22 @@ public class Movimento3D : MonoBehaviour
     [Tooltip("Câmera que será usada para")]
     public Camera cam;
 
-    [Tooltip("Nome do input para mover o objeto para frente e para trás")]
-    [SerializeField] private string inputHorizontal;
-    [Tooltip("Nome do input para mover o objeto para esquerda e direita")]
-    [SerializeField] private string inputVertical;
+    private string inputHorizontal = "Horizontal";
+    private string inputVertical = "Vertical";
 
-    [Tooltip("A força aplicada para manter o jogador colado ao chão em uma descida enclinada")]
-    public float forcaRampa;
-    [Tooltip("O comprimento do raio que checa se o jogador está em uma rampa ou não")]
-    public float comprimentoRaioForcaRampa;
+    private float forcaRampa = 5f;
+    private float comprimentoRaioForcaRampa = 2f;
 
     [Tooltip("A taxa de diminuição de força que o pulo sofrerá até seu ápice")]
     public AnimationCurve decrescimentoDoPulo;
     [Tooltip("A 'força' do pulo")]
-    public float multiplicadorPulo;
+    public float forcaPulo;
     [Tooltip("A tecla usada para pular")]
     public KeyCode teclaDePulo;
 
+    [Tooltip("Quão suave será a rotação do personagem")]
+    public float turnSmoothVelocity;
+    float turnSmoothTime;
 
     private bool isJumping;
 
@@ -36,27 +35,44 @@ public class Movimento3D : MonoBehaviour
     private void Awake()
     {
         charController = GetComponent<CharacterController>();
-        cam = GetComponentInChildren<Camera>();
+        if(cam == null)
+            cam = GetComponentInChildren<Camera>();
     }
 
+    float horizInput;
+    float vertInput;
+    Vector3 direcaoFrente = Vector3.zero;
     private void Update()
     {
-        float horizInput = Input.GetAxis(inputHorizontal);
-        float vertInput = Input.GetAxis(inputVertical);
+        horizInput = Input.GetAxis(inputHorizontal);
+        vertInput = Input.GetAxis(inputVertical);
 
-        charController.SimpleMove(Vector3.ClampMagnitude((vertInput * cam.transform.forward +
-                                    horizInput * cam.transform.right) * velocidadeMovimento , 1.0f) * velocidadeMovimento);
+        Debug.Log(cam.transform.forward);
+
+        if (Input.GetKeyDown(teclaDePulo) && !isJumping)
+        {
+            isJumping = true;
+            StartCoroutine(JumpEvent());
+        }
+        else
+        {
+
+            if(horizInput != 0 || vertInput != 0)
+            {
+                float targetRotation = Mathf.Atan2(horizInput, vertInput) * Mathf.Rad2Deg + cam.transform.eulerAngles.y;
+                transform.eulerAngles = Vector3.up * Mathf.SmoothDampAngle(transform.eulerAngles.y, targetRotation, ref turnSmoothVelocity, turnSmoothTime);
+                if (vertInput < 0)
+                    vertInput = vertInput * -1;
+            }
+            charController.SimpleMove(Vector3.ClampMagnitude((vertInput * transform.forward +
+                                        horizInput * cam.transform.right), 1.0f) * velocidadeMovimento);
+        }
 
         if((horizInput != 0 || vertInput != 0) && CheckSlope())
         {
             charController.Move(Vector3.down * charController.height / 2 * forcaRampa * Time.deltaTime);
         }
 
-        if(Input.GetKeyDown(teclaDePulo) && !isJumping)
-        {
-            isJumping = true;
-            StartCoroutine(JumpEvent());
-        }
     }
 
     private bool CheckSlope()
@@ -66,7 +82,7 @@ public class Movimento3D : MonoBehaviour
 
         RaycastHit hit;
 
-        if (Physics.Raycast(transform.position, Vector3.down, out hit, charController.height / 2 * comprimentoRaioForcaRampa))
+        if (Physics.Raycast(transform.position, Vector3.down, out hit, charController.height / 2f * comprimentoRaioForcaRampa))
             if (hit.normal != Vector3.up)
                 return true;
 
@@ -75,16 +91,19 @@ public class Movimento3D : MonoBehaviour
 
     private IEnumerator JumpEvent()
     {
+        charController.slopeLimit = 90.0f;
         float timeInAir = 0f;
         do
         {
             float jumpForce = decrescimentoDoPulo.Evaluate(timeInAir);
-            charController.Move(Vector3.up * jumpForce * multiplicadorPulo * Time.deltaTime);
+            charController.Move((Vector3.ClampMagnitude((vertInput * transform.forward +
+                                    horizInput * cam.transform.right), 1.0f) * velocidadeMovimento  
+                                    + Vector3.up *jumpForce * forcaPulo) * Time.deltaTime);
             timeInAir += Time.deltaTime;
             yield return null;
         } while (!charController.isGrounded && charController.collisionFlags != CollisionFlags.Above);
 
-        charController.slopeLimit = 90.0f;
+        charController.slopeLimit = 45.0f;
         isJumping = false;
     }
 }
