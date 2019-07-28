f7ed9b094d85f922af0f3bacacb40e7040f98518
Bernardo <behornerl@gmail.com>
2019-07-28 12:39:58 -0300

    Pequeno update script de pontuação

--- Oficina Introdução Unity/Assets/Scripts/Pontuacao.cs
+++ Oficina Introdução Unity/Assets/Scripts/Pontuacao.cs
@@ -5,18 +5,21 @@ using UnityEngine.UI;
 
 public class Pontuacao : MonoBehaviour
 {
-    private int pontuacao;
+    private float pontuacao;
 
-    [Tooltip("Lista de textos que vão apresentar a pontuação do jogador")]
+    [Tooltip("Lista de componentes Text que vão apresentar a pontuação do jogador")]
     public List<Text> displaysPontuacao = new List<Text>();
 
     private void Update()
     {
         foreach(Text texto in displaysPontuacao)
-        texto.text = pontuacao.ToString();
+        {
+            if(texto != null)
+                texto.text = pontuacao.ToString();
+        }
     }
 
-    public void AumentarPontuacao(int incremento)
+    public void AumentarPontuacao(float incremento)
     {
         pontuacao += incremento;
     }
